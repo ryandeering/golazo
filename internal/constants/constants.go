91169764351e570b546b011fe3899c37d8dc65b5
Juanma Roca <juanmaxroca@gmail.com>
2025-12-14 15:05:42 -0500

    fix[constants]: reduce main view spinner delay to 0.5 seconds

--- internal/constants/constants.go
+++ internal/constants/constants.go
@@ -3,4 +3,4 @@ package constants
 import "time"
 
 // MainViewCheckDelay is the delay before navigating to a selected view in the main menu.
-const MainViewCheckDelay = 1500 * time.Millisecond // 1.5 seconds
+const MainViewCheckDelay = 500 * time.Millisecond // 0.5 seconds
